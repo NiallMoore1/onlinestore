c376e9075082afc5138415c2b4a70bf87c82b8b5
niall moore <moore.nialls@gmail.com>
2019-10-21 20:56:28 +0100

    end of youtube video

--- js/main.js
+++ js/main.js
@@ -8,3 +8,13 @@ $(".slider-one")
     prevArrow:".site-slider .slider-btn .prev",
     nextArrow:".site-slider .slider-btn .next"
 });
+
+/*second slider*/ 
+$(".slider-two")
+.not(".slick-intialized")
+.slick({
+    prevArrow:".site-slider-two.prev",
+    nextArrow:".site-slider-two.next",
+    slidesToShow:5,
+    slidesToShow:1
+});
